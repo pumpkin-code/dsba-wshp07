1dc4aed624f789bb224b753eabd7a607aaf7937f
Sergey Sh <bjolyk@gmail.com>
2019-11-21 21:09:59 +0300

    Solution by K.Rudakov added.

--- src/tasks/main.cpp
+++ src/tasks/main.cpp
@@ -31,13 +31,104 @@
  ******************************************************************************/
 
 #include <iostream>
-// TODO: include all necessary headers
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <random>
 
 
-// TODO: implement all necessary methods
+
+typedef std::vector<std::string> VecNames;
+
+VecNames toCountSurvived(std::istream& input_file);
+void printVec(const VecNames& aliveNames);
+std::string genThreeDigitNumber(const int& random_state);
 
 int main ()
 {
-    // TODO: put all the stuff together
-    return 0;
+    const std::string INP_FILE_NAME = "/Users/kirill/Documents/Кирилл Рудаков/Учеба/Преподавание/C++/untitled1/tasks/input.csv";
+    std::ifstream input_file;
+    input_file.open(INP_FILE_NAME);
+
+    VecNames aliveNames = toCountSurvived(input_file);
+    printVec(aliveNames);
+
+
+    VecNames newVec;
+
+    int size = aliveNames.size();
+
+    while (size > 0)
+    {
+        newVec.push_back(genThreeDigitNumber(size));
+        size--;
+    }
+
+    printVec(newVec);
+}
+
+std::vector<std::string> toCountSurvived(std::istream& input_file)
+{
+    std::string line;
+    VecNames aliveNames;
+
+    while (!input_file.eof() && input_file.good ())
+    {
+        std::getline (input_file, line);
+        std::stringstream ss(line);
+        std::string token;
+
+        int token_i = 0;
+        bool isSurvived = false;
+        while (!ss.eof () && ss.good ())
+        {
+            std::getline (ss, token,',');
+
+            if (token_i == 1)
+            {
+                short survived;
+                std::stringstream t(token);
+                t >> survived;
+                if (survived == 1)
+                    isSurvived = true;
+            }
+
+            if (token_i == 3 && isSurvived)
+            {
+                std::string surname;
+                std::stringstream surStream(token);
+                std::getline(surStream, surname,';');
+                aliveNames.push_back(surname);
+            }
+
+            ++token_i;
+        }
+    }
+
+    return aliveNames;
+}
+
+
+void printVec(const VecNames& aliveNames)
+{
+    int i = 1;
+    for (std::string name : aliveNames)
+        std::cout << i++ << ") " << name<<'\n';
+}
+
+std::string genThreeDigitNumber(const int& random_state){
+    std::mt19937 gen;
+    gen.seed(random_state);
+    std::uniform_int_distribution<int> distr_0_9(0,9);
+    std::uniform_int_distribution<int> distr_1_9(1,9);
+
+    std::stringstream numberStream;
+    for (int i = 0; i < 3; i++)
+        numberStream <<  (i > 0 ? distr_0_9(gen) : distr_1_9 (gen));
+
+    std::string number;
+    std::getline(numberStream,number);
+    return number;
+
+
 }
